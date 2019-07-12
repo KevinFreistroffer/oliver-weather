f0baa733bc49c113f0fb4e830a5e3fe4f1dfd152
Kevin <kevin.freistroffer@gmail.com>
2019-07-11 17:30:46 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, './static')));
+
+app.use('/*', (req, res) => {
+	res.sendFile(express.static(path.join(__dirname, './index.html')));
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Listeningfds on port: ${process.env.PORT}`);		 		
+});
+
+
+module.exports = app;
+ 
---
+++ precache-manifest.4606ef6c3c0bbd0f85dcf40bddaf6173.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d597224620e9dd3df2eee3c0f9be83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c86a6422796f710107",
+    "url": "/static/css/main.16298f39.chunk.css"
+  },
+  {
+    "revision": "3bd2bcc0ecfc3fabf548",
+    "url": "/static/js/2.9d28795b.chunk.js"
+  },
+  {
+    "revision": "81c86a6422796f710107",
+    "url": "/static/js/main.0d9ead36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
